6f042e55b0c5b487edb3ff2517ee90074e6ff40d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-07 12:01:38 -0600

    begin building grid layout

--- client/public/js/controllers.js
+++ client/public/js/controllers.js
@@ -10,41 +10,4 @@ app.controller('mainController', function($scope, $http) {
         console.log('Error: ' + error);
       });
   };
-
-
-  // $scope.createVehicle = function() {
-  //   $http.post('/api/v1/vehicles/', $scope.formData)
-  //     .success(function(data) {
-  //       $scope.formData = {}; // clears the form every entry
-  //       $scope.vehicles.push(data);
-  //       console.log(data);
-  //     })
-  //     .error(function(error) {
-  //       console.log('Error: ' + error);
-  //     });
-  // };
-  //
-  // $scope.deleteVehicle = function(id) {
-  //   $http.delete('/api/v1/vehicles/' + id)
-  //     .success(function(data) {
-  //       $scope.vehicles.splice(data, 1);
-  //       console.log(data);
-  //     })
-  //     .error(function(error) {
-  //       console.log('Error: ' + error);
-  //     });
-  // };
-  //
-  //   $scope.editVehicle = function(id) {
-  //   $http.put('/api/v1/vehicles/' + id, $scope.modalData)
-  //     .success(function(data) {
-  //       data = $scope.modalData;
-  //       console.log(data)
-  //       // $route.reload()
-  //       $scope.$apply();
-  //     })
-  //     .error(function(error) {
-  //       console.log('Error: ' + error);
-  //     });
-  // };
 });
--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,8 +3,6 @@ var map;
 function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 39.179751, lng: -104.987866},
     mapTypeId: google.maps.MapTypeId.TERRAIN
   });
 
@@ -18,13 +16,19 @@ function initMap() {
   }
 
   var trail = new google.maps.Polyline({
-    path: google.maps.geometry.encoding.decodePath('y`knFt}okSLLbBJPDNNPFf@CNOXGXAd@Bn@HVEt@]b@BVNp@x@d@TBl@YbAWBSKa@_AS]g@Gg@Ji@Fg@ROZG^FlAD\\?d@YP@d@b@EROLWHm@Dq@NaATFO|@TL^Lf@d@f@Pv@Hh@ITKNSFUd@iC?_@H[ZLz@D^JXRJXXd@TLh@Hp@`AXBXCb@XB^a@JQJk@CYOS_@oBi@[ESRn@PVr@ZZr@fBFj@UhA?b@Gl@k@EkAwAg@LAr@Uv@a@l@Id@Mb@A`@PFJVBt@E\\]KMc@QWW`C?v@Hj@{@`@e@~@]`AQVa@HSJWPQ`@'),
+    path: buildPoly(a),
     geodesic: true,
-    strokeColor: '#FF0000',
+    strokeColor: '#0D0',
     strokeOpacity: 1.0,
-    strokeWeight: 2
+    strokeWeight: 3
   });
 
   trail.setMap(map);
   zoomToTrail(trail);
+}
+
+var a = 'y`knFt}okSLLbBJPDNNPFf@CNOXGXAd@Bn@HVEt@]b@BVNp@x@d@TBl@YbAWBSKa@_AS]g@Gg@Ji@Fg@ROZG^FlAD\\?d@YP@d@b@EROLWHm@Dq@NaATFO|@TL^Lf@d@f@Pv@Hh@ITKNSFUd@iC?_@H[ZLz@D^JXRJXXd@TLh@Hp@`AXBXCb@XB^a@JQJk@CYOS_@oBi@[ESRn@PVr@ZZr@fBFj@UhA?b@Gl@k@EkAwAg@LAr@Uv@a@l@Id@Mb@A`@PFJVBt@E\\]KMc@QWW`C?v@Hj@{@`@e@~@]`AQVa@HSJWPQ`@';
+
+function buildPoly(pathString) {
+  return pathString ? google.maps.geometry.encoding.decodePath(pathString) : null;
 }
