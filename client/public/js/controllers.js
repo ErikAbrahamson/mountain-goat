cef8ef6c090692318cb077c5e8088034c95acce6
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-08 14:04:20 -0600

    refine search feature

--- client/public/js/controllers.js
+++ client/public/js/controllers.js
@@ -1,8 +1,15 @@
 app.controller('mainController', function($scope, $http) {
 
+  $scope.hasError = false;
+
   $scope.getSegments = function(bounds) {
     $http.post('/api/service', { 'bounds': bounds })
       .success(function(data) {
+        if (data.segments.length === 0) {
+          $scope.hasError = true;
+        } else {
+          $scope.hasError = false;
+        }
         $scope.trails = data.segments;
       });
   };
--- client/public/js/directives/search.js
+++ client/public/js/directives/search.js
@@ -7,21 +7,19 @@ app.directive('segmentSearch', function() {
     link: function($scope, element, attrs) {
 
       var geocoder = new google.maps.Geocoder();
-
       $scope.codeAddress = function() {
+
         var address = document.getElementById('address').value;
         geocoder.geocode({ 'address': address }, function(results, status) {
+          console.log(results[0]);
           if (status == google.maps.GeocoderStatus.OK) {
-
             var swLat = results[0].geometry.bounds.Qa.J,
                 swLon = results[0].geometry.bounds.Ma.j,
                 neLat = results[0].geometry.bounds.Qa.j,
                 neLon = results[0].geometry.bounds.Ma.J;
-
             var bounds = swLat + ',' + swLon + ',' + neLat + ',' + neLon;
 
-            return $scope.getSegments(bounds)
-            // get latlong, build bounds, send to /service query
+            return $scope.getSegments(bounds);
           } else {
             alert('Error: ' + status);
           }
