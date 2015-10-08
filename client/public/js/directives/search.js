cb8e331aa29d1e7156e41308d4d83f0f3f63b7d9
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-08 15:31:01 -0600

    handle search object variation

--- client/public/js/directives/search.js
+++ client/public/js/directives/search.js
@@ -10,18 +10,37 @@ app.directive('segmentSearch', function() {
       $scope.codeAddress = function() {
 
         var address = document.getElementById('address').value;
+
         geocoder.geocode({ 'address': address }, function(results, status) {
-          console.log(results[0]);
+
           if (status == google.maps.GeocoderStatus.OK) {
-            var swLat = results[0].geometry.bounds.Qa.J,
-                swLon = results[0].geometry.bounds.Ma.j,
-                neLat = results[0].geometry.bounds.Qa.j,
-                neLon = results[0].geometry.bounds.Ma.J;
-            var bounds = swLat + ',' + swLon + ',' + neLat + ',' + neLon;
+
+            console.log(results[0]);
+
+            // if (!results[0].geometry.hasOwnProperty('bounds')) $scope.hasError = true;
+            if (results[0].geometry.hasOwnProperty('bounds')) {
+
+              var swLat = results[0].geometry.bounds.Qa.J,
+                  swLon = results[0].geometry.bounds.Ma.j,
+                  neLat = results[0].geometry.bounds.Qa.j,
+                  neLon = results[0].geometry.bounds.Ma.J;
+                  bounds = swLat + ',' + swLon + ',' + neLat + ',' + neLon;
+
+              return $scope.getSegments(bounds);
+
+            } else {
+
+              var swLatV = results[0].geometry.viewport.Qa.J,
+                  swLonV = results[0].geometry.viewport.Ma.j,
+                  neLatV = results[0].geometry.viewport.Qa.j,
+                  neLonV = results[0].geometry.viewport.Ma.J;
+                  bounds = swLatV + ',' + swLonV + ',' + neLatV + ',' + neLonV;
 
             return $scope.getSegments(bounds);
+          }
+
           } else {
-            alert('Error: ' + status);
+            $scope.hasError = true;
           }
         });
       };
