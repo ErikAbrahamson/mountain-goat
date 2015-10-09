1fd8b3351e393f6ff22a68a9004283631fdd5df0
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-09 16:04:40 -0600

    add view booleans

--- client/public/js/controllers.js
+++ client/public/js/controllers.js
@@ -1,7 +1,15 @@
 app.controller('mainController', function($scope, $http) {
 
+  $scope.view = { thumbs: true, single: false };
+
   $scope.hasError = false;
   $scope.location = { search: '' };
+  $scope.currentTrail = { currentID: ''};
+
+  $scope.showTrail = function() {
+    $scope.view.thumb = false;
+    $scope.view.single = true;
+  };
 
   $scope.getSegments = function(bounds) {
     $scope.loading = true;
