13dd774fc6eb09d797f2211ceebab81454987ebf
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-11-22 16:42:30 -0700

    update file structure

--- src/client/controllers/logout.js
+++ src/client/controllers/logout.js
@@ -1,17 +1,11 @@
-angular.module('myApp').controller('logoutController',
-  ['$scope', '$location', 'AuthService',
+angular.module('myApp').controller('logoutController', [
+  '$scope', '$location', 'AuthService',
   function ($scope, $location, AuthService) {
-
     $scope.logout = function () {
 
-      console.log(AuthService.getUserStatus());
-
-      // call logout from service
       AuthService.logout()
         .then(function () {
           $location.path('/login');
         });
-
     };
-
 }]);
