302a40301f16aed50bc5750c85becaad9c7abe08
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-11-22 17:00:41 -0700

    update main app syntax

--- src/client/main.js
+++ src/client/main.js
@@ -1,21 +1,21 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute'])
 
-myApp.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {templateUrl: 'partials/home.html'})
-    .when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'})
-    .when('/logout', {controller: 'logoutController'})
-    .when('/register', {templateUrl: 'partials/register.html', controller: 'registerController'})
-    .when('/one', {template: '<h1>This is page one!</h1>'})
-    .when('/two', {template: '<h1>This is page two!</h1>'})
-    .otherwise({redirectTo: '/'});
-});
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {templateUrl: 'partials/home.html'})
+      .when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'})
+      .when('/logout', {controller: 'logoutController'})
+      .when('/register', {templateUrl: 'partials/register.html', controller: 'registerController'})
+      .when('/one', {template: '<h1>This is page one!</h1>'})
+      .when('/two', {template: '<h1>This is page two!</h1>'})
+      .otherwise({redirectTo: '/'});
+  })
 
-myApp.run(function ($rootScope, $location, $route, AuthService) {
-  $rootScope.$on('$routeChangeStart', function (event, next, current) {
-    if (next.access.restricted && AuthService.isLoggedIn() === false) {
-      $location.path('/login');
-      $route.reload();
-    }
+  .run(function ($rootScope, $location, $route, AuthService) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+      if (next.access.restricted && AuthService.isLoggedIn() === false) {
+        $location.path('/login');
+        $route.reload();
+      }
+    });
   });
-});
